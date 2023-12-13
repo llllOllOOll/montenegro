eb4e9e5b511583836bcc4cf382e1cb58bcda521e
llllOllOOll <7b37b3@gmail.com>
2023-12-13 18:52:36 -0300

    Contact info added

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,40 +2,40 @@ import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
+    { title: "Colégio Montenegro" },
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+   <>
+   <div className="flex flex-col justify-center content-center h-screen">
+    <h1 className="text-blue-900 text-center font-bold text-6xl">Colégio Montenegro</h1>
+    
+    <div className="flex flex-col justify-center py-16">
+    <h2 className="text-center font-bold text-2xl">Contato</h2>
+
+    <div className="flex justify-center gap-8 py-6 font-medium">
+      <div className="flex gap-2">
+    <img className="w-6 h-6" src="https://cdn-icons-png.flaticon.com/128/732/732200.png" alt="mail"></img>
+    <p>contato@colegiomontenegro.com.br</p>
+      </div>
+
+      <div className="flex gap-2">
+    <img className="w-6 h-6" src="https://cdn-icons-png.flaticon.com/128/2111/2111463.png" alt="instagram"></img>
+    <p>@colegiomontenegro_</p>
+      </div>
+
+      <div className="flex gap-2">
+    <img className="w-6 h-6" src="https://cdn-icons-png.flaticon.com/128/597/597177.png" alt="phone"></img>
+    <p>(75)992331534</p>
     </div>
+
+    </div>
+    </div>
+
+    </div>
+   </> 
   );
 }
